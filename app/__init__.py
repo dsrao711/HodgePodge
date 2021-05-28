2f7a1768d8a8ce13fbdff4d9db19547297b9bd51
Divya <dsrao0712@gmail.com>
2021-05-29 01:53:34 +0530

    Login,register,logout

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,41 @@
+  
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
 from app.config import Config
 
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
+mail = Mail()
+
+
+    
 def create_app(config_class=Config):
     app = Flask(__name__)
     
-    app.config.from_object(Config)
+    app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
     
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+    
+    
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    
+    
+
     from app.users.routes import users
     from app.main.routes import main
-    
-    
     app.register_blueprint(users)
     app.register_blueprint(main)
-    
+
     return app
--- run.py
+++ run.py
@@ -1,8 +1,12 @@
-from app import create_app
+from app import create_app,db
 
 app = create_app()
 
+db.create_all(app=create_app())
+
+
 if __name__ == '__main__':
+    
     app.run(debug=True)
     
     
