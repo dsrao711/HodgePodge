966be57ebab0d6bf854453b1de5691f6ae3e25aa
Yogesh Upadhyay <yogeshh2021@gmail.com>
2021-05-30 02:24:34 +0530

    chat completed

--- app/static/main.js
+++ app/static/main.js
@@ -22,19 +22,48 @@ const socket = io();
 
 socket.on("message", (data) => {
     console.log(data);
-    var ele = document.getElementById("msg")
 
-    ele.innerHTML = `${ele.innerHTML}
-    <div class="flex-row d-flex">
-        <div class="alert alert-success"  style="max-width: 40% ; height : 7vh ">
-            <p>${data.msg}</p>
-        </div>
-    </div>`
+    if (data.msg) {
+        var ele = document.getElementById("msg")
+
+        ele.innerHTML = `${ele.innerHTML}
+        <div class="flex-row d-flex">
+            <div class="alert alert-success"  style="max-width: 40% ; height : 7vh ">
+                <p>${data.msg}</p>
+            </div>
+        </div>`
+    }
+    
 });
 
 function chatapp(name) {
-    var ele = document.getElementById("chat-header");
-    ele.innerHTML = name;
+    var chatName = document.getElementById("chat-header");
+    chatName.innerHTML = name;
+
+    var ele = document.getElementById("msg");
+    var messages = JSON.parse(localStorage.getItem("messages"));
+    console.log(messages);
+
+    if (messages) {
+        var prevName = localStorage.getItem("currChat");
+        messages[prevName] = ele.innerHTML;
+
+        if (messages[name]) {
+            ele.innerHTML = messages[name];
+        } else {
+            ele.innerHTML = "";
+        }
+        localStorage.setItem("messages", JSON.stringify(messages));
+    } else {
+        var prevName = localStorage.getItem("currChat")
+        if (prevName) {
+            var messages = {};
+            messages[prevName] = ele.innerHTML;
+        }
+        console.log(messages)
+        ele.innerHTML = ""
+        localStorage.setItem("messages", JSON.stringify(messages));
+    }
 
     localStorage.setItem("currChat", name);
 }
